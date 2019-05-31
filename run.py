0ec1424c6b4afaefe51b41c0780a6d1bd086bd52
TimeFinger <hewn2011@126.com>
2019-06-01 00:08:25 +0800

    添加测试repo

--- run.py
+++ run.py
@@ -4,8 +4,11 @@ import os
 import time
 from git import Repo
 
-repo = Repo('./')
-git = repo.git
-git.add(os.path.join('./', __file__))
-git.commit(message='提交自己测试')
-git.push()
+repo_dir = os.path.join('./', 'example-repo')
+repo = Repo.init(repo_dir)
+with open(os.path.join(repo_dir, 'records.txt'), 'w'):
+    pass
+#git = repo.git
+#git.add(os.path.join('./', __file__))
+#git.commit(message='提交自己测试')
+#git.push()
