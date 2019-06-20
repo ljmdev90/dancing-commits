f431894d6f9c4c06b1b4776129a864362680757a
TimeFinger <hewn2011@126.com>
2019-06-20 22:42:30 +0800

    初始化目标仓库

--- dancing.py
+++ dancing.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python3
+import os
 import sys
 import getopt
 
+from git import Repo
+
 class Dancing():
-    def __init__(repo, mode):
+    
+    branch_name = 'dancing_master'
+    file_name = '.dancing.log'
+
+    def __init__(self, repo, mode):
+        self.repo = Repo.init(repo)
+        os.chdir(repo)
+        with open(self.file_name, 'a+') as f:
+            if len(self.repo.heads) == 0:   # 如果是空仓库, 则进行一次提交，因为不提交的话不能创建新的分支
+                self.repo.index.add(items=[self.file_name])
+                self.repo.index.commit('first commit')
+            
+            branch = self.repo.create_head(self.branch_name)    # 创建一个名为dancing_master的分支
+            branch.checkout()
+
+        self.mode = mode
+
+    def run(self):
         pass
+        
 
 if __name__ == "__main__":
     opts, args = getopt.getopt(sys.argv[1:], "r:m:", ["repo=", "mode="])
@@ -14,5 +35,6 @@ if __name__ == "__main__":
             repo = val
         elif key in ("-m", "--mode"):
             mode = val
-    print(repo, mode)
+    dancing = Dancing(repo, mode)
+    dancing.run()
             
