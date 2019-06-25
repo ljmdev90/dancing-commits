6f18218838c0b16791e47412d5d55f52f6e4f364
TimeFinger <hewn2011@126.com>
2019-06-25 22:55:43 +0800

    完善text类型的提交

--- dancing.py
+++ dancing.py
@@ -4,6 +4,7 @@ import sys
 import getopt
 from datetime import datetime
 from random import randint
+from bisect import bisect
 
 from git import Repo
 from PIL import Image, ImageDraw, ImageFont
@@ -18,10 +19,12 @@ class Dancing():
     def __init__(self, repo):
         self.repo = Repo.init(repo)
         os.chdir(repo)
+        self.is_empty_repo = False
         with open(self.file_name, 'a+') as f:
             if len(self.repo.heads) == 0:   # 如果是空仓库, 则进行一次提交，因为不提交的话不能创建新的分支
                 self.repo.index.add(items=[self.file_name])
                 self.repo.index.commit('first commit')
+                self.is_empty_repo = True
             
             branch = self.repo.create_head(self.branch_name)    # 创建一个名为dancing_master的分支
             branch.checkout()
@@ -30,21 +33,22 @@ class Dancing():
         if mode not in self.modes:
             raise ValueError
 
+        year = kargs.get('year')
+        dt_end = datetime.now() if year is None else datetime(int(year), 12, 31)
+        week_day = dt_end.weekday()
+        lask_week_days = 0 if week_day == 6 else week_day + 1
+        dt_start = datetime.fromtimestamp(dt_end.timestamp() - (52 * 7 + lask_week_days) * 3600 * 24)
+        days = dt_end - dt_start
+        date_iter = (datetime.fromtimestamp(dt_start.timestamp() + d * 24 * 3600).date() for d in range(days.days))
+        
         week_map = [[] for _ in range(7)]
+
+        commit_dates = {}  # 原来的提交记录
+        for commit in self.repo.iter_commits():
+            k = str(commit.committed_datetime.date())
+            commit_dates.setdefault(k, 1)
+            commit_dates[k] += 1
         if mode == 'random':
-            year = kargs.get('year')
-            dt_end = datetime.now() if year is None else datetime(int(year), 12, 31)
-            week_day = dt_end.weekday()
-            lask_week_days = 0 if week_day == 6 else week_day + 1
-            dt_start = datetime.fromtimestamp(dt_end.timestamp() - (52 * 7 + lask_week_days) * 3600 * 24)
-            days = dt_end - dt_start
-            date_iter = (datetime.fromtimestamp(dt_start.timestamp() + d * 24 * 3600).date() for d in range(days.days))
-            
-            commit_dates = {}  # 原来的提交记录
-            for commit in self.repo.iter_commits():
-                k = str(commit.committed_datetime.date())
-                commit_dates.setdefault(k, 1)
-                commit_dates[k] += 1
             with open(self.file_name, 'a+') as f:
                 for i, d in enumerate(date_iter):
                     cnt = 0 
@@ -83,21 +87,28 @@ class Dancing():
                 x = int((im_width - width) / 2)
             draw.text((x, y), text, font=font)
             px = im.load()
-            px_index = [(j, i) for i in range(7) for j in range(53)]
-            for i in px_index:
-                x = int(px[i])
-                cnt = 0
-                if x <= 50:
-                    cnt = 4
-                elif 50 < x <= 100:
-                    cnt = 3
-                elif 100 < x <= 150:
-                    cnt = 2
-                elif 150 < x <= 200:
-                    cnt = 1
-                else:
-                    cnt = 0
-                week_map[i[1] % 7].append(cnt)
+            px_index = [(i, j) for i in range(53) for j in range(7)]
+            for i, d in enumerate(date_iter):
+                x = int(px[px_index[i]])
+                cnt = 4 - bisect([50, 100, 150, 200], x)
+                if cnt > 0:
+                    if str(d) in commit_dates:
+                        cnt += commit_dates[str(d)]
+                    else:
+                        commit_datetimes = sorted(
+                            [datetime(d.year, d.month, d.day, randint(0, 23), randint(0, 59), randint(0, 59)) for _ in range(5)],
+                            reverse=True
+                        )
+                        with open(self.file_name, 'a+') as f:
+                            for n in range(cnt):
+                                msg = '第' + str(n+1) + '次提交\n'
+                                commit_datetime = commit_datetimes.pop()
+                                f.write(str(commit_datetime.date()) + ' ' + msg)
+                                f.read()
+                                self.repo.index.add([self.file_name])
+                                commit_datetime = str(int(float(commit_datetime.timestamp()))) + ' +0800'
+                                self.repo.index.commit(message=msg, author_date=commit_datetime, commit_date=commit_datetime)
+                week_map[px_index[i][1]].append(cnt)
             self.preview(week_map)
             # im.resize((530, 70)).show()
 
